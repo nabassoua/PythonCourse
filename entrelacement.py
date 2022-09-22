8eafb21cfc478241da266af532f7e6d566fc6b15
Mohamed Nabassoua <mnabassoua@gmail.com>
2022-09-22 13:17:12 -0400

    no comments...

--- entrelacement.py
+++ entrelacement.py
@@ -4,7 +4,14 @@ def entrelacement1(s1, s2):
 
     for i in range(len(s1)):
         s += s1[i] + s2[i]
+        print(s)
     return s
 
 
 print(entrelacement1('abc', '123'))
+
+# output
+# a1
+# a1b2
+# a1b2c3
+# a1b2c3
